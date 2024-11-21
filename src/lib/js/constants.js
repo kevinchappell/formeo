f9b124e383ec24c1d72c4fd1058dffee24c7d243
Kevin Chappell <kevin@chappell.dev>
2024-11-20 18:50:25 -0800

    chore: update formeo-languages

--- src/lib/js/constants.js
+++ src/lib/js/constants.js
@@ -166,7 +166,7 @@ export const CONDITION_INPUT_ORDER = [
 
 export const FIELD_PROPERTY_MAP = {
   value: 'attrs.value',
-  checked: 'attrs.checked',
+  isChecked: 'attrs.checked',
   ...visiblityConfigs,
 }
 
